8e233e4c1c7142089e8e1dacc182f259fb81f1ab
Mat <matrj08@gmail.com>
2019-10-19 02:35:22 -0600

    Parse
    Se agrega la opcion de parsear archivos .docx y .txt

--- src/text/finder/Controller.java
+++ src/text/finder/Controller.java
@@ -5,18 +5,23 @@
  */
 package text.finder;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Arrays;
+import java.util.List;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.regex.Pattern;
 import javafx.event.ActionEvent;
+import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -26,6 +31,10 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.FileChooser;
+import org.apache.poi.hwpf.HWPFDocument;
+import org.apache.poi.hwpf.extractor.WordExtractor;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 
 /**
  *
@@ -38,7 +47,9 @@ import javafx.stage.FileChooser;
 public class Controller implements Initializable  {
 
     Lista listaFiles = new Lista();
+    Lista listaParsedFiles = new Lista();
     private int filesNames = 0 ;
+    private String lastSelected;
     @FXML
     Button addFile;
     @FXML
@@ -48,18 +59,43 @@ public class Controller implements Initializable  {
     @FXML
     Pane filesPane;
 
+    
     @Override
     public void initialize(java.net.URL location, ResourceBundle resources) {
 
         /**
          *  
          * @param event
-         * @see https://docs.oracle.com/cd/E17802_01/javafx/javafx/1.3/docs/api/javafx.scene/doc-files/cssref.html#typeeffect
-         * @see http://lineadecodigo.com/java/como-ejecutar-un-comando-del-sistema-desde-java/
          */
-        
         addFile.setOnAction((ActionEvent event) -> {
+            addFile();
+        });
+        
+        parse.setOnAction((ActionEvent event) -> {
+            parseFile();
+        });
+        
+      
+     
+        
+    }
+   
+    
+    
+    /**
+     * 
+     * @see https://docs.oracle.com/cd/E17802_01/javafx/javafx/1.3/docs/api/javafx.scene/doc-files/cssref.html#typeeffect
+     * @see http://lineadecodigo.com/java/como-ejecutar-un-comando-del-sistema-desde-java/
+     */  
+    private void addFile(){
+        
             FileChooser fileChooser = new FileChooser();
+            fileChooser.getExtensionFilters().addAll(
+                    new ExtensionFilter("TXT", "*.txt"),
+                    new ExtensionFilter("PDF", "*.pdf"),
+                    new ExtensionFilter("DOCX", "*.docx")
+            );
+            
             File selectedFile = fileChooser.showOpenDialog(TextFinder.getStage());
             String data = selectedFile.toString();
             System.out.println(data);
@@ -69,49 +105,64 @@ public class Controller implements Initializable  {
                         
             data = dataAux[dataAux.length -1];
             listaFiles.insertAtLast(data);
+            
             try {
-                copyFileUsingStream(selectedFile, data);
+                
+                copyFile(selectedFile, data);
+                
             } catch (IOException ex) {
+                
                 System.err.print(ex);
                 Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
+                
             }
             
             addElements();
-//            try {
-//                String [] cmd = {"copy " + selectedFile.toString() + "  ..\\library "}; //Comando de apagado en windows
-//                Runtime.getRuntime().exec(cmd);
-//            } catch (IOException ioe) {
-//                System.out.println (ioe);
-//            }
-        });
-     
         
+       
+    }   
+    
+    
+    /**
+     * 
+     * https://www.tutorialspoint.com/create-a-new-empty-file-in-java
+     * https://www.journaldev.com/861/java-copy-file
+     * @param source
+     * @param stringDest
+     * @throws IOException 
+     */    
+    private static void copyFile(File source, String stringDest) throws IOException {
+       
+        InputStream is = null;
+        OutputStream os = null;
         
+        try {      
+
+            File dest = new File("src\\library\\"+stringDest);
+            dest.createNewFile();
+            is = new FileInputStream(source);
+            os = new FileOutputStream(dest);
+            byte[] buffer = new byte[1024];
+            int length;
+
+            while ((length = is.read(buffer)) > 0) {
+
+                os.write(buffer, 0, length);
+
+            }
+
+        } finally {
+
+            is.close();
+            os.close();
+
+        }
     }
     
-    private static void copyFileUsingStream(File source, String stringDest) throws IOException {
-       
-    InputStream is = null;
-    OutputStream os = null;
-    try {        
-        File dest = new File("src\\library\\"+stringDest);
-        dest.createNewFile();
-        is = new FileInputStream(source);
-        os = new FileOutputStream(dest);
-        byte[] buffer = new byte[1024];
-        int length;
-        while ((length = is.read(buffer)) > 0) {
-            os.write(buffer, 0, length);
-        }
-    } finally {
-        is.close();
-        os.close();
-    }
-}
     
     private void addElements(){
         
-        Nodo current = listaFiles.getHead();          
+        NodoLista current = listaFiles.getHead();          
         double y = 10;
         filesPane.getChildren().clear();
         
@@ -134,6 +185,11 @@ public class Controller implements Initializable  {
                 labelFile.setStyle("-fx-background-color: rgba(255, 255, 255, 0.5);");
             });
             
+            labelFile.setOnMouseClicked((MouseEvent event) -> {
+                lastSelected = labelFile.getText();
+                System.out.println("Did you click me?");
+            });
+            
             filesPane.getChildren().add(labelFile);
             current = current.getNext();
             y += 30;
@@ -141,4 +197,82 @@ public class Controller implements Initializable  {
         }
 
     }
+    
+    private void parseFile(){
+        String FieldDelimiter = " ";
+ 
+        
+        if(lastSelected.contains("txt")){        
+            BufferedReader br;
+            try {
+
+                br = new BufferedReader(new FileReader("src\\library\\"+lastSelected.replace("  ", "")));
+                String line;     
+                
+
+                while ((line = br.readLine()) != null) {
+                    String[] fields = line.split(FieldDelimiter, -1);
+                    insertLineInTree(fields);
+                }
+
+                
+
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(TextFinder.class.getName())
+                        .log(Level.SEVERE, null, ex);
+
+            } catch (IOException ex) {
+                Logger.getLogger(TextFinder.class.getName())
+                        .log(Level.SEVERE, null, ex);
+            }
+            
+            
+            
+        }else if(lastSelected.contains("docx")){
+            
+            try {
+                File file = new File("src\\library\\"+lastSelected.replace("  ", ""));
+                FileInputStream fis = new FileInputStream(file.getAbsolutePath());
+
+                XWPFDocument document = new XWPFDocument(fis);
+
+                List<XWPFParagraph> paragraphs = document.getParagraphs();
+
+
+                for (XWPFParagraph para : paragraphs) {
+                    
+                    String sPara = para.getText().toString();
+                    String[] fields = sPara.split(FieldDelimiter, -1);
+                    insertLineInTree(fields);
+//                    System.out.println(para.getText());
+                }
+                fis.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        
+    }
+    
+    public void insertLineInTree(String[] fields){
+        Arbol arbol = new Arbol();
+            for(int i = 0; i < fields.length; i++){
+                arbol.insert(fields[i]);
+                System.out.println(fields[i]+" Added");
+            }
+        listaParsedFiles.insertAtLast(arbol);
+        inOrder(arbol.getRoot());
+    }
+    
+    
+    public void inOrder(NodoArbol root) {
+        if(root !=  null) {
+            inOrder(root.getIzq());
+            //Visit the node by Printing the node data  
+            System.out.println(root.getData());
+            inOrder(root.getDer());
+        }
+    }
+
+
 }
